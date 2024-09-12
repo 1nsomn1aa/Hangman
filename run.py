81341d58952b87fcba310ec0bbf38951f93c3116
tomas Serpenskas <serpenskas.tomas@gmail.com>
2024-09-12 12:31:16 +0000

    Add a check for correct guesses and display underscores for incorrect ones

--- run.py
+++ run.py
@@ -9,3 +9,17 @@ class Hangman:
         self.word = random.choice(self.words)
         self.guesses = ""
         self.turns = 10
+
+    def print_current_game_state(self):
+        """
+        Iterate through the random word 
+        Display underscores for letters
+        Replace underscores with correctly guessed letters
+        """
+        for letter in self.word:
+            if letter in self.guesses:
+                print(letter)
+            else:
+                print('_')
+
+
